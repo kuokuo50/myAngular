31dba69ab87bac454eb222d94b4ce5cb7d905cad
1400376 <hebekuo@systex.com>
2020-03-19 10:14:33 +0800

    MRTStationList

---
+++ MRTStationList/src/app/station-list.const.ts
@@ -0,0 +1,17 @@
+export const stationList = [
+  {
+    'Station': '三重國小站',
+    'Destination': '南勢角站',
+    'UpdateTime': '2018-10-15T22:14:41.557'
+  },
+  {
+    'Station': '士林站',
+    'Destination': '大安站',
+    'UpdateTime': '2018-10-15T22:14:35.497'
+  },
+  {
+    'Station': '士林站',
+    'Destination': '淡水站',
+    'UpdateTime': '2018-10-15T22:14:25.347'
+  }
+];
